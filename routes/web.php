7669e2bd882add713cd22821270c0c29765beb1d
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-26 20:36:25 +0900

    Install tymon/jwt-auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
