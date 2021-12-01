b76a36e860ff85a589cd0f8f2539796a98ba3782
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-12-01 23:21:06 +0900

    Modify AuthController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'auth'
+], function() {
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::group([
+    'prefix' => 'auth',
+    'middleware' => 'auth:api'
+], function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
