c52285efc09b3480022d88ca1a185a7ca862aecf
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-15 01:18:37 +0900

    Change path

--- resources/js/src/components/templates/SignInView.vue
+++ resources/js/src/components/templates/SignInView.vue
@@ -2,7 +2,7 @@
   <SignInCard />
 </template>
 <script>
-import SignInCard from "../organisms/SignInCard";
+import SignInCard from "components/organisms/SignInCard";
 export default {
   name: "SignInView",
   components: {
