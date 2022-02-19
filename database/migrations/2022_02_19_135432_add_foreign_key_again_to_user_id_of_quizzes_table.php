4c415719592aaea3b933f0c14dbefd6532523540
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-19 22:56:02 +0900

    Add user_id to Quiz model

---
+++ database/migrations/2022_02_19_135432_add_foreign_key_again_to_user_id_of_quizzes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyAgainToUserIdOfQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            //
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            //
+        });
+    }
+}
