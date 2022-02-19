e22361b1ccf42aa456800871799d8b33dc65b984
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-19 22:48:12 +0900

    Add foreign_quiz_id to Choice model

---
+++ database/migrations/2022_02_19_134644_add_foreign_key_to_quiz_id_of_choices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToQuizIdOfChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('choices', function (Blueprint $table) {
+            //
+            $table->dropColumn('quiz_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('choices', function (Blueprint $table) {
+            //
+            $table->integer('quiz_id')->unsigned();
+            $table->foreign('quiz_id')->references('id')->on('quizzes')->onDelete('CASCADE');
+        });
+    }
+}
