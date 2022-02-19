39c08322fa0fdeb89e9b043271204ed418214952
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-19 22:23:06 +0900

    Add belongs to

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+
+    protected $guarded = array('id');
+    
+    public static $rules = array(
+        'user_id' => 'required',
+        'title' => 'required',
+        'query' => 'required',
+        'explanation' => 'required'
+    );
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
