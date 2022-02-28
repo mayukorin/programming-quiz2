0104ba8f4aaa50521831fa2d4d3ade98ddcb0f28
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-28 18:01:19 +0900

    fix:add requresAuth: true to make sure that only logged-in users can create quiz

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -34,7 +34,8 @@ const routes = [
     {
       path: "/quizzes/new",
       name: "QuizCreate",
-      component: QuizCreate
+      component: QuizCreate,
+      meta: { requiresAuth: true }
     },
     {
       path: "*",
