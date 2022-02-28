9b8b5a20c6e976c23363f296eab1eac40b55b9eb
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-02-28 17:48:59 +0900

    feat:add quiz store function

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Quiz;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreQuiz;
 
 class QuizController extends Controller
 {
@@ -20,5 +22,18 @@ class QuizController extends Controller
     {
         $quiz = Quiz::with(['user', 'correct_choice', 'choices'])->findOrFail($id);
         return response()->json($quiz, 200);
+    }
+
+    public function store(StoreQuiz $request)
+    {
+
+        $quiz = auth()->user()->quizzes()->create($request->quiz);
+        foreach($request->choices as $choice) {
+            $choice = $quiz->choices()->create($choice);
+            if ($choice->number == intval($request->correct_choice_number)) {
+                $quiz->update(['correct_choice_id' => $choice->id]);
+            }
+        }
+        return response()->json($quiz, 202);
     }
 }
