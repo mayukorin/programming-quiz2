c541d59dd4b956fbc7d6cfc1fb7f78d09412021f
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-03-06 15:54:37 +0900

    refactor:modify title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+        <title>PRO-QUIZ</title>
 
         <script src="{{ mix('js/app.js') }}" defer></script>
 
